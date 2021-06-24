8496fe143f80bcfd7f0b7a67f2d6db7c090880c2
Gabriel <gaba929@gmail.com>
2021-06-23 19:38:09 -0500

    Fix job submition

--- Test/exe.sh
+++ Test/exe.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Starting job on " `date` #Date/time of start of job
-echo "Running on: `uname -a`" #Condor job is running on this node
-echo "System software: `cat /etc/redhat-release`" #Operating System on that node
+echo "Hello World!"
+#echo "Starting job on " `date` #Date/time of start of job
+#echo "Running on: `uname -a`" #Condor job is running on this node
+#echo "System software: `cat /etc/redhat-release`" #Operating System on that node
 
 #source /cvmfs/cms.cern.ch/cmsset_default.sh
 #cd /eos/user/g/gayalasa/CondoBase/CMSSW_10_6_12/src/myAnalyzers/BtoKsMuMu
